daa176537091e23072847f186bafc01fbc25b56d
Oscar Pereira <PereiraO28@student.jhs.net>
2026-09-10 09:58:36 -0700

    Implement Distance calculation in Distance.java
    Fixes issues with input reading and distance calculation.

---
+++ Unit 2/Distance.java
@@ -0,0 +1,19 @@
+Import java.utils.*;
+
+public class Distance {
+  public static void main(String[] args);
+  Scaner readIn = new Scabber(System.in);
+
+    System.out.print("Enter x1: ");
+    double x1 = readIn.nextDouble();
+  
+    System.out.print("Enter y1: ");
+    double y1 = readIn.nextDouble();
+  
+    System.out.print("Enter x2: ");
+    double x2 = readIn.nextDouble();
+  
+    System.out.print("Enter y2: ");
+    double y2 = readIn.nextDouble();
+  
+    double dist - Math.sqrt(Math.pow(x1-x1, 2) +Math.pow(y2-y1, 2));

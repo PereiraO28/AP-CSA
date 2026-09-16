3a69e74f3bbbbaa06f18acfd728f92ed0ceefb8d
Oscar Pereira <PereiraO28@student.jhs.net>
2026-09-16 11:22:44 -0700

    Create Kinematics.java

---
+++ Unit 2/Kinematics.java
@@ -0,0 +1,27 @@
+import java.utils*;
+
+public class Kinematics {
+  public static void main(String[] args) {
+    Scanner readIn = new Scanner(System.in);
+
+    System.out.print("Enter the initial velocity: ");
+    double vi = readIn.nextDouble();
+
+    System.out.print("Enter the time: ");
+    double t = readIn.nextDouble();
+
+    System.out.print("Enter the acceleration: ");
+    double a = readIn.nextDouble();
+
+    double delta_x = calcDisplacement(vi, t, a);
+    System.out.println(delta_x);
+    
+  }
+  
+  public static double calcDisplacement(double v_initial, double time, double acceleration) {
+    double dx = v_initial * time + 0.5 * a * Math.pow(t, 2);
+    return dx;
+    
+  }
+  
+}

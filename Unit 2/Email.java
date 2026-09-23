ffd2c454f9fb664765e3579770103fdf6c728652
Oscar Pereira <PereiraO28@student.jhs.net>
2026-09-23 12:15:09 -0700

    Create Email class to generate student emails
    Fix input reading and email generation logic.

---
+++ Unit 2/Email.java
@@ -0,0 +1,20 @@
+public class Email {
+  public static void main(String[] args) {
+
+    Scanner readIn = new Scanner(System.in);
+    
+    System.out.print("Enter the student's first name: ");
+    String first_name = readIn.nextDouble();
+
+    System.out.print("Enter the student's last name: ");
+    String last_name = readIn.nextDouble();
+
+    System.out.print("Enter the student's graduating year: ");
+    int year = readIn.nextDouble();
+
+    year %=100
+
+    String email = last_name
+    System.out.println(email);
+  }
+}
